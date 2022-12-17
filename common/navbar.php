1413bee4bd0c11d738ef8d06f7c8809ee41a2f5e
GeoAlva <alvarado.geovanny.98@gmail.com>
2022-12-17 17:37:51 +0100

    inital files

---
+++ common/navbar.php
@@ -0,0 +1,31 @@
+
+
+<div class="navbar">
+        <ul>
+            <a href="index.php"><img src="images/carrefive.png" alt="carrefive logo"></a>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Team</a></li>
+            <!-- <li><a href="#">Case history</a></li>
+            <li><a href="#">Work with us</a></li>
+            <li><a href="#">Contacts</a></li> -->
+            
+            <?php
+            if(isset($_COOKIE["utente"])){
+                echo "<li class=\"signin\"><a href=\"../php/logout.php\"> Log Out</a></li>";
+                echo "<li class=\"signin\"><a href=\"#\"> Ciao, ".$_COOKIE["utente"]."</a></li>";
+                
+            }
+            else{
+                echo "<li class=\"signin\"><a href=\"signup.php\"> Sign Up</a></li>";
+                echo "<li class=\"signin\"><a href=\"login.php\"> Log In</a></li>";
+            }
+            ?>
+            <li class="searchbar">
+                <form action="#" method="get" >
+                    <input type="text" name="search" id="search" placeholder="Search Product...">
+                    <button type="submit" for="search">Search</button>   
+                </form>     
+            </li>
+            
+        </ul>
+</div>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Carrefive</title>
+</head>
+<body class="bg">
+    <?php
+    include "common/navbar.php";
+    ?>
+    <div ></div>
+    
+    
+    <footer class="foot">
+ Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+ Quis error sapiente adipisci blanditiis deserunt, 
+ officia aperiam quam voluptas eveniet ducimus nostrum facere ipsa nihil quod suscipit 
+ nemo quibusdam ipsum doloribus?
+</footer>
+
+
+</body>
+</html>
