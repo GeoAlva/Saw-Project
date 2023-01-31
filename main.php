56e4aef175c55bfc43b52618b5708ffa4cc2c7f1
Leonardo Caviglione <S4818508@studenti.unige.it>
2023-01-31 10:17:45 +0100

    aggiunti tasti quantita javascript

--- main.php
+++ main.php
@@ -22,16 +22,21 @@
                     <img src="images/61gYYxGLDfL._AC_SX679_.jpg" alt="pasta">
                     <h5>pasta a</h5>
                     <div>5.99$</div> 
-                    <button type="submit"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
+                    <div class="quantity_div">
+                        <button type="button" onclick="decreaseQt('pr1')" class="minus_button" pattern="[1-9][0-9]?" name="minus_button">-</button>    
+                        <input type="text" id="pr1" class="quantity" name="quantita" value="1">
+                        <button type="button" onclick="increaseQt('pr1')" class="plus_button" name="plus_button">+</button>
+                    </div>
+                    <button type="submit" class="cart_button"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
                 </div>
                 <div class="shop_elem">
                     <img src="images/61gYYxGLDfL._AC_SX679_.jpg" alt="pasta">
                     <h5>pasta b</h5>
                     <div>5.99$</div>   
                     <div class="quantity_div">
-                        <button type="button" class="minus_button" name="minus_button">-</button>    
-                        <input type="text" class="quantity" name="quantita" value="1">
-                        <button type="button" class="plus_button" name="plus_button">+</button>
+                        <button type="button" onclick="decreaseQt('pr2')" class="minus_button" pattern="[1-9][0-9]?" name="minus_button">-</button>  
+                        <input type="text" id="pr2" class="quantity" name="quantita" value="1">
+                        <button type="button" onclick="increaseQt('pr2')" class="plus_button" name="plus_button">+</button>
                     </div>
                     <button type="submit" class="cart_button"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
                 </div>
@@ -39,13 +44,23 @@
                     <img src="images/61gYYxGLDfL._AC_SX679_.jpg" alt="pasta">
                     <h5>pasta c</h5>
                     <div>5.99$</div> 
-                    <button type="submit"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
+                    <div class="quantity_div">
+                        <button type="button" onclick="decreaseQt('pr3')" class="minus_button" pattern="[1-9][0-9]?" name="minus_button">-</button>   
+                        <input type="text" id="pr3" class="quantity" name="quantita" value="1">
+                        <button type="button" onclick="increaseQt('pr3')" class="plus_button" name="plus_button">+</button>
+                    </div>
+                    <button type="submit" class="cart_button"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
                 </div>
                 <div class="shop_elem">
                     <img src="images/61gYYxGLDfL._AC_SX679_.jpg" alt="pasta">
                     <h5>pasta d</h5>
                     <div>5.99$</div> 
-                    <button type="submit"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
+                    <div class="quantity_div">
+                        <button type="button" onclick="decreaseQt('pr4')" class="minus_button" pattern="[1-9][0-9]?" name="minus_button">-</button>    
+                        <input type="text" id="pr4" class="quantity" name="quantita" value="1">
+                        <button type="button" onclick="increaseQt('pr4')" class="plus_button" name="plus_button">+</button>
+                    </div>
+                    <button type="submit" class="cart_button"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
                 </div>
             </div>
         </div>
@@ -57,49 +72,89 @@
                     <img src="images/61gYYxGLDfL._AC_SX679_.jpg" alt="pasta">
                     <h5>pasta a</h5>
                     <div>5.99$</div> 
-                    <button type="submit"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
+                    <div class="quantity_div">
+                        <button type="button" onclick="decreaseQt('pr5')" class="minus_button" pattern="[1-9][0-9]?" name="minus_button">-</button>  
+                        <input type="text" id="pr5" class="quantity" name="quantita" value="1">
+                        <button type="button" onclick="increaseQt('pr5')" class="plus_button" name="plus_button">+</button>
+                    </div>
+                    <button type="submit" class="cart_button"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
                 </div>
                 <div class="shop_elem">
                     <img src="images/61gYYxGLDfL._AC_SX679_.jpg" alt="pasta">
                     <h5>pasta b</h5>
                     <div>5.99$</div> 
-                    <button type="submit"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
+                    <div class="quantity_div">
+                        <button type="button" onclick="decreaseQt('pr6')" class="minus_button" pattern="[1-9][0-9]?" name="minus_button">-</button>   
+                        <input type="text" id="pr6" class="quantity" name="quantita" value="1">
+                        <button type="button" onclick="increaseQt('pr6')" class="plus_button" name="plus_button">+</button>
+                    </div>
+                    <button type="submit" class="cart_button"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
                 </div>
                 <div class="shop_elem">
                     <img src="images/61gYYxGLDfL._AC_SX679_.jpg" alt="pasta">
                     <h5>pasta c</h5>
                     <div>5.99$</div> 
-                    <button type="submit"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
+                    <div class="quantity_div">
+                        <button type="button" onclick="decreaseQt('pr7')" class="minus_button" pattern="[1-9][0-9]?" name="minus_button">-</button>   
+                        <input type="text" id="pr7" class="quantity" name="quantita" value="1">
+                        <button type="button" onclick="increaseQt('pr7')" class="plus_button" name="plus_button">+</button>
+                    </div>
+                    <button type="submit" class="cart_button"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
                 </div>
                 <div class="shop_elem">
                     <img src="images/61gYYxGLDfL._AC_SX679_.jpg" alt="pasta">
                     <h5>pasta d</h5>
                     <div>5.99$</div> 
-                    <button type="submit"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
+                    <div class="quantity_div">
+                        <button type="button" onclick="decreaseQt('pr8')" class="minus_button" pattern="[1-9][0-9]?" name="minus_button">-</button>    
+                        <input type="text" id="pr8" class="quantity" name="quantita" value="1">
+                        <button type="button" onclick="increaseQt('pr8')" class="plus_button" name="plus_button">+</button>
+                    </div>
+                    <button type="submit" class="cart_button"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
                 </div>
                 <div class="shop_elem">
                     <img src="images/61gYYxGLDfL._AC_SX679_.jpg" alt="pasta">
                     <h5>pasta d</h5>
                     <div>5.99$</div> 
-                    <button type="submit"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
+                    <div class="quantity_div">
+                        <button type="button" onclick="decreaseQt('pr9')" class="minus_button" pattern="[1-9][0-9]?" name="minus_button">-</button>   
+                        <input type="text" id="pr9" class="quantity" name="quantita" value="1">
+                        <button type="button" onclick="increaseQt('pr9')" class="plus_button" name="plus_button">+</button>
+                    </div>
+                    <button type="submit" class="cart_button"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
                 </div>
                 <div class="shop_elem">
                     <img src="images/61gYYxGLDfL._AC_SX679_.jpg" alt="pasta">
                     <h5>pasta d</h5>
                     <div>5.99$</div> 
-                    <button type="submit"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
+                    <div class="quantity_div">
+                        <button type="button" onclick="decreaseQt('pr10')" class="minus_button" pattern="[1-9][0-9]?" name="minus_button">-</button>    
+                        <input type="text" id="pr10" class="quantity" name="quantita" value="1">
+                        <button type="button" onclick="increaseQt('pr10')" class="plus_button" name="plus_button">+</button>
+                    </div>
+                    <button type="submit" class="cart_button"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
                 </div>
                 <div class="shop_elem">
                     <img src="images/61gYYxGLDfL._AC_SX679_.jpg" alt="pasta">
                     <h5>pasta d</h5>
                     <div>5.99$</div> 
-                    <button type="submit"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
+                    <div class="quantity_div">
+                        <button type="button" onclick="decreaseQt('pr11')" class="minus_button" pattern="[1-9][0-9]?" name="minus_button">-</button>    
+                        <input type="text" id="pr11" class="quantity" name="quantita" value="1">
+                        <button type="button" onclick="increaseQt('pr11')" class="plus_button" name="plus_button">+</button>
+                    </div>
+                    <button type="submit" class="cart_button"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
                 </div>
                 <div class="shop_elem">
                     <img src="images/61gYYxGLDfL._AC_SX679_.jpg" alt="pasta">
                     <h5>pasta d</h5>
                     <div>5.99$</div> 
-                    <button type="submit"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
+                    <div class="quantity_div">
+                        <button type="button" onclick="decreaseQt('pr12')" class="minus_button" pattern="[1-9][0-9]?" name="minus_button">-</button>   
+                        <input type="text" id="pr12" class="quantity" name="quantita" value="1">
+                        <button type="button" onclick="increaseQt('pr12')" class="plus_button" name="plus_button">+</button>
+                    </div>
+                    <button type="submit" class="cart_button"><i class="fa-sharp fa-solid fa-cart-plus"></i></button>
                 </div>
 
             </div>
@@ -109,5 +164,22 @@
     <?php
         include "common/footer.php";
     ?>
+
+<script>
+    function increaseQt(id){
+    var elem = document.getElementById(id);
+    var input = elem.value;
+    if(input < 1) elem.value = 1;
+    input < 99 ?  elem.value = Number(input)+1 : elem.value = 99;
+    };
+
+    function decreaseQt(id){
+    var elem = document.getElementById(id);
+    var input = elem.value;
+    if(input > 99) elem.value = 99;
+    else
+        input > 1 ?  elem.value = Number(input)-1 : elem.value = 1;
+    };
+</script>
 </body>
 </html>
